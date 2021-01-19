58ef18a909fd953f6884092c1182542dc98b5fad
Wyatt Barnes <me@wyatt.email>
2021-01-19 15:55:10 -0600

    Merge conflict

--- packages/web3-eth2-beaconchain/src/index.ts
+++ packages/web3-eth2-beaconchain/src/index.ts
@@ -1,17 +1,8 @@
-// @ts-ignore - types not full implemented yet
-import { ETH2Core, IBaseAPISchema, ETH2BaseOpts } from 'web3-eth2-core'
+import { ETH2Core, BaseAPISchema, ETH2BaseOpts } from 'web3-eth2-core'
 import { DefaultSchema } from './schema'
 
 import { ETH2BeaconChain as IETH2BeaconChain, IBlockExplorerApi } from '../types/index'
 import { BlockExplorerApi } from './blockExplorerApi'
-// @ts-ignore - types not full implemented yet
-import { IBaseAPISchema, ETH2BaseOpts } from 'web3-eth2-core'
-
-// @ts-ignore Typescript isn't aware that class methods are added during execution
-import { ETH2Core, BaseAPISchema, ETH2BaseOpts } from 'web3-eth2-core'
-import { DefaultSchema } from './schema'
-
-import { ETH2BeaconChain as IETH2BeaconChain } from '../types/index'
 
 // @ts-ignore - ETH2BeaconChain incorrectly implements interface IETH2BeaconChain
 // because methods are added during runtime
